f2898976eee242328d562a4116a1832ff65ffd36
Grant Birkinbine <grantbirki@github.com>
2022-03-05 23:01:05 -0800

    add config

---
+++ script/config.py
@@ -0,0 +1,47 @@
+import json
+from jinja2 import Template
+import sys
+
+HTTP_MONITOR_INTERVAL = 120
+REPO_DIR = sys.argv[1]
+
+def http_template():
+    with open(f"{REPO_DIR}/config/templates/http_template.json") as f:
+        config = f.read()
+    return config
+
+def base_template():
+    with open(f"{REPO_DIR}/config/templates/base.json") as f:
+        base = json.loads(f.read())
+    return base
+
+def domains():
+    with open(f"{REPO_DIR}/config/domains.txt") as f:
+        domains = f.read().splitlines()
+    return domains
+
+def write_config(base, monitors):
+    with open(f"{REPO_DIR}/config/config.json", "w") as f:
+        base["monitorList"] = monitors
+        f.write(json.dumps(base))
+
+def generate_http_monitors(domains):
+    monitors = []
+    template = Template(http_template())
+    
+    for domain in domains:
+        monitor = template.render(domain=domain, interval=HTTP_MONITOR_INTERVAL)
+        monitors.append(json.loads(monitor))
+    return monitors
+
+def main():
+
+    all_monitors = []
+
+    http_monitors = generate_http_monitors(domains())
+
+    all_monitors += http_monitors
+    write_config(base_template(), all_monitors)
+
+if __name__ == "__main__":
+    main()
